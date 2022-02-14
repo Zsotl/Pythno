1292154e5f3481eac663ca36ef26e451980fc5a4
Zsotl <b0papzso@vasvari.org>
2022-02-14 12:18:55 +0100

    xd

--- feladat_01/main.py
+++ feladat_01/main.py
@@ -9,6 +9,8 @@ import os
 
 halmaz: List[int] = []
 elemekSzama: int = None
+osszeg: int = None
+ketJegyuSzamok: int = None
 
 def hiba(text: str) -> None:
     print(text)
@@ -39,13 +41,30 @@ def halmazKiirasa(kiirandoHalmaz: List[int]) -> None:
     for item in kiirandoHalmaz:
         print(f"{item}", end="\t")
 
-"""
-1. feladat:  Írassuk ki a tartalmát fordított sorrendben
-"""
 def halmazKiirasaForditva(kiirandoHalmaz: List[int]) -> None:
     for i in range(len(kiirandoHalmaz)-1, -1, -1):
         print(f"{kiirandoHalmaz[i]}", end="\t")
-    
+
+def parosSzamokKiirasa(kiirandoHalmaz: List[int]) -> None:
+    for item in kiirandoHalmaz:
+        if(item % 2 == 0):
+            print(f"{item}", end="\t")
+
+def sum(osszeadandoHalmaz: List[int]) -> int:
+    eredmeny: int = 0
+
+    for item in osszeadandoHalmaz:
+        eredmeny += item
+
+    return eredmeny
+
+def ketJegyuSzamokSzama(keresesHalmaza: List[int])-> int:
+    eredmeny: int = 0
+    for item in keresesHalmaza:
+        if(abs(item) > 9 and abs(item) < 100):
+            eremdeny += 1
+
+    return eredmeny
 
 #fopgrogram
 elemekSzama = elemSzamBekeres() 
@@ -54,5 +73,34 @@ halmaz = listaFeltoltese(elemekSzama)
 os.system("cls")
 print("A halmaz elemei: \n")
 halmazKiirasa(halmaz)
+"""
+1. feladat:  Írassuk ki a tartalmát fordított sorrendben
+"""
 print("\nA halmaz elemei fordítva: \n")
 halmazKiirasaForditva(halmaz)
+
+"""
+2. feladat: Számítsuk ki az elemek összegét
+"""
+
+osszeg = sum(halmaz)
+print(f"\nA halmaz elemeinek összege: {osszeg}")
+
+"""
+3. feladat: Átlagoljuk a tömbelemeket
+"""
+
+print(f"\nA halmaz elemeinek átlaga: {osszeg / elemekSzama}")
+
+"""
+4. feladat: írassuk ki a páros elemeket
+"""
+
+print(f"\nA halmaz páros elemei: ")
+parosSzamokKiirasa(halmaz)
+
+"""
+5. feladat: Számoljuk meg, hogy hány két jegyű szám van a tömbben
+"""
+ketJegyuSzamok = ketJegyuSzamokSzama(halmaz)
+print(f"\nA kétjegyű számok száma: {ketJegyuSzamok}")
